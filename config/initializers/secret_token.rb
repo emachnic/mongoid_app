9fcace7469b5ca981460fd8c95d146bc46935c1f
Evan Machnic <emachnic@broadmac.net>
2011-07-07 10:03:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidApp::Application.config.secret_token = 'c06abac514e1618bb696ba35c9f0238b2489a4e38f60cdd298ce55ba6d031518358a70455d902ea2a42dba475cc661b188483df5ddfca56de6993f50b30b34e9'
